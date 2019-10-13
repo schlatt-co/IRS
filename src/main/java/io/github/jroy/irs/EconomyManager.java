8d72025954dff5aaffcbcdfaeb72c4ad50e10c67
JRoy <joshroy126@gmail.com>
2019-10-12 21:58:08 -0400

    Add transaction logging

--- src/main/java/io/github/jroy/irs/EconomyManager.java
+++ src/main/java/io/github/jroy/irs/EconomyManager.java
@@ -3,9 +3,11 @@ package io.github.jroy.irs;
 import dev.tycho.stonks.managers.DatabaseHelper;
 import dev.tycho.stonks.model.core.Account;
 import dev.tycho.stonks.model.core.AccountLink;
+import dev.tycho.stonks.model.logging.Transaction;
 import net.ess3.api.events.UserBalanceUpdateEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -37,25 +39,27 @@ public class EconomyManager implements Listener {
       if (event.getPlayer().isOnline()) {
         event.getPlayer().sendMessage(ChatColor.AQUA + "IRS>> " + ChatColor.YELLOW + "You've been taxed 10% of your incoming $" + difference.intValue() + " ($" + difference.multiply(incomingTaxRate).intValue() + ")");
       }
-      depositTaxAccount(difference.multiply(incomingTaxRate).intValue());
+      depositTaxAccount(event.getPlayer(), difference.multiply(incomingTaxRate).intValue());
       return event.getNewBalance().subtract(difference.multiply(incomingTaxRate).round(new MathContext(1)));
     } else if (difference.intValue() > 100000) { //Receiving party is getting over 100k
       if (event.getPlayer().isOnline()) {
         event.getPlayer().sendMessage(ChatColor.AQUA + "IRS>> " + ChatColor.YELLOW + "You've been taxed 7% of your incoming $" + difference.intValue() + " ($" + difference.multiply(outgoingTaxRate).intValue() + ")");
       }
-      depositTaxAccount(difference.multiply(outgoingTaxRate).intValue());
+      depositTaxAccount(event.getPlayer(), difference.multiply(outgoingTaxRate).intValue());
       return event.getNewBalance().subtract(difference.multiply(outgoingTaxRate).round(new MathContext(1)));
     } else { //Tax-free transaction
       return event.getNewBalance();
     }
   }
 
-  private void depositTaxAccount(double amount) {
+  private void depositTaxAccount(Player player, double amount) {
     Optional<AccountLink> optional = DatabaseHelper.getInstance().getCompanyByName("Admins").getAccounts().stream().filter(accountLink -> accountLink.getAccount().getName().equalsIgnoreCase("Taxes")).findAny();
     if (optional.isPresent()) {
-      Account account = optional.get().getAccount();
+      AccountLink accountLink = optional.get();
+      Account account = accountLink.getAccount();
       account.addBalance(amount);
       DatabaseHelper.getInstance().getDatabaseManager().updateAccount(account);
+      DatabaseHelper.getInstance().getDatabaseManager().logTransaction(new Transaction(accountLink, player.getUniqueId(), "Incoming Transaction Tax of " + player.getName(), amount));
       return;
     }
     Bukkit.getLogger().severe("Tax account optional is not present!");
